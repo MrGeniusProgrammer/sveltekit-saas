99fd64edf84043e45acd70fb96b43f953d408e3e
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-11-26 21:19:09 +0300

    script to setup lemonsqueezy webhooks

--- src/lib/server/use-cases/payment.ts
+++ src/lib/server/use-cases/payment.ts
@@ -1,18 +1,6 @@
 import type { UserId } from "@/entities/user";
-import { env } from "@/env";
-import { logger } from "@/helpers/logger";
-import { lemonSqueezySetup } from "@lemonsqueezy/lemonsqueezy.js";
 import type { DiscriminatedWebhookPayload } from "lemonsqueezy-webhooks";
-
-const configureLemonSqueezy = () => {
-	lemonSqueezySetup({
-		apiKey: env.LEMONSQUEEZY_API_KEY,
-		onError: (error) => {
-			logger.fatal(error, `Lemon Squeezy API error: ${error.message}`);
-			throw new Error(`Lemon Squeezy API error: ${error.message}`);
-		},
-	});
-};
+import { configureLemonSqueezy } from "../lemonsqueezy";
 
 interface ProcessPaymentWebhookPayloadParams {
 	payload: DiscriminatedWebhookPayload<{
---
+++ src/routes/(landing)/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import {
+		Card,
+		CardContent,
+		CardHeader,
+		CardTitle,
+	} from "@/components/ui/card";
+	import { CircleCheck, CircleX } from "lucide-svelte";
+</script>
+
+<div class="flex h-screen w-screen items-center justify-center">
+	<div class="flex w-full max-w-xl flex-wrap gap-2.5">
+		<Card>
+			<CardHeader>
+				<CardTitle>Basic</CardTitle>
+			</CardHeader>
+			<CardContent class="flex flex-col justify-center gap-1.5">
+				<div class="flex items-center gap-2">
+					<CircleCheck class="size-6" />
+					<p class="text-lg leading-tight">Simple</p>
+				</div>
+				<div class="flex items-center gap-2">
+					<CircleCheck class="size-6" />
+					<p class="text-lg leading-tight">Simple</p>
+				</div>
+				<div class="flex items-center gap-2">
+					<CircleCheck class="size-6" />
+					<p class="text-lg leading-tight">Simple</p>
+				</div>
+				<div class="flex items-center gap-2">
+					<CircleCheck class="size-6" />
+					<p class="text-lg leading-tight">Simple</p>
+				</div>
+			</CardContent>
+		</Card>
+	</div>
+</div>
