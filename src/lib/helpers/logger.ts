2e625cb8df2bed7745e02d95dbf5d4332d347065
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-11-28 00:18:55 +0300

    simplified logic for excluding pino-pretty from production

--- src/lib/helpers/logger.ts
+++ src/lib/helpers/logger.ts
@@ -2,51 +2,32 @@ import { env } from "@/env";
 import pino from "pino";
 import { effectTaskEitherBoth } from "./fp-ts";
 
-const rootLogger =
-	env.NODE_ENV === "development"
-		? pino({
-				level: env.LOG_LEVEL,
-				browser: {
-					disabled: globalThis.window === undefined,
-				},
-				transport: {
+const rootLogger = pino({
+	level: env.LOG_LEVEL,
+	browser: {
+		disabled: globalThis.window === undefined,
+	},
+	transport:
+		env.NODE_ENV === "development"
+			? {
 					target: "pino-pretty",
-				},
-				redact: [
-					"*.password",
-					"*.hashedPassword",
-					"*.passwordHash",
-					"*.session.id",
-					"password",
-					"passwordHash",
-					"hashedPassword",
-					"*.oldPassword",
-					"*.newPassword",
-					"oldPassword",
-					"newPassword",
-				],
-				nestedKey: "payload",
-			})
-		: pino({
-				level: env.LOG_LEVEL,
-				browser: {
-					disabled: globalThis.window === undefined,
-				},
-				redact: [
-					"*.password",
-					"*.hashedPassword",
-					"*.passwordHash",
-					"*.session.id",
-					"password",
-					"passwordHash",
-					"hashedPassword",
-					"*.oldPassword",
-					"*.newPassword",
-					"oldPassword",
-					"newPassword",
-				],
-				nestedKey: "payload",
-			});
+				}
+			: undefined,
+	redact: [
+		"*.password",
+		"*.hashedPassword",
+		"*.passwordHash",
+		"*.session.id",
+		"password",
+		"passwordHash",
+		"hashedPassword",
+		"*.oldPassword",
+		"*.newPassword",
+		"oldPassword",
+		"newPassword",
+	],
+	nestedKey: "payload",
+});
 
 export const getLogSuccessMessage = (message: string) => `${message} succeeded`;
 export const getLogErrorMessage = (message: string) => `${message} failed`;
