42198619ec8c245521fa65be8e7da566fdd2108e
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-12-05 23:15:20 +0300

    modularized some stuff

--- src/routes/docs/[...slug]/toc.svelte
+++ src/routes/docs/[...slug]/toc.svelte
@@ -1,26 +1,10 @@
 <script lang="ts">
-	import {
-		SidebarMenuSub,
-		SidebarMenuSubItem,
-	} from "@/components/ui/sidebar";
 	import type { Toc } from "@/entities/toc";
-	import Self from "./toc.svelte";
+	import TocItem from "./toc-item.svelte";
 
 	let { items }: { items: Toc } = $props();
 </script>
 
 {#each items as item (item.id)}
-	<SidebarMenuSubItem>
-		<a
-			href={`#${item.id}`}
-			class="text-muted-foreground hover:text-foreground"
-		>
-			{item.text}
-		</a>
-		{#if item.items && item.items.length > 0}
-			<SidebarMenuSub class="mx-1 px-3 py-1.5">
-				<Self items={item.items} />
-			</SidebarMenuSub>
-		{/if}
-	</SidebarMenuSubItem>
+	<TocItem {item} />
 {/each}
